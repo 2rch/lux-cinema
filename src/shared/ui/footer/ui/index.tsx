5a5397339a57a48c2a7fe42309fd3ccb7133ca38
Alex Turchynov <oleksandr.turchynov@nixsolutions.com>
2023-09-28 23:30:16 +0300

    Added social media icons

--- src/shared/ui/footer/ui/index.tsx
+++ src/shared/ui/footer/ui/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./index.scss";
 import { useTranslation } from "react-i18next";
+import { IconLinks } from "../../icon-links";
 
 export const Footer = () => {
   const { t } = useTranslation();
   return (
     <footer className={"footer"}>
-      {t("common.footer.title", { currentYear: new Date().getFullYear() })}
+      <div className={"footer-text"}>
+        {t("common.footer.title", { currentYear: new Date().getFullYear() })}
+      </div>
+      <IconLinks />
     </footer>
   );
 };
